18826d89bf9bc1600ea192c416602ce4f629b612
thomas <thomasyacoub321@gmail.com>
2025-05-09 19:32:08 +0300

    changed the paernt of the search form

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
 import "../css/styles.css";
 import form from "./components/Form";
 
-document.getElementById("root").append(form);
+document.querySelector("section.search .container").append(form);
